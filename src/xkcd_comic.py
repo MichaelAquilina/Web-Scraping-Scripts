a096bca6aa3b00a5084d5d67f1117a696b4b4e78
KillaW0lf04 <michaelaquilina@gmail.com>
2013-01-10 19:14:20 +0100

    Improvements and code refactoring

--- src/xkcd_comic.py
+++ src/xkcd_comic.py
@@ -2,27 +2,38 @@ __author__ = 'michaela'
 
 from MemHTMLParser import MemHTMLParser
 import urllib2
+import argparse
 
 WEBSITE_URL = 'http://xkcd.com/'
 
 if __name__ == '__main__':
-    target_url = WEBSITE_URL
-    save_location_img = 'xkcd.jpg'
+    parser = argparse.ArgumentParser("Download XKCD Comics over command line")
+    parser.add_argument('-f', '--file', type=str, required=True, help="Specify the filename to save to")
+    parser.add_argument('-c', '--comic-id', type=int, required=False, help="Specify a specific comic to download by id")
+    
+    args = parser.parse_args()
+    
+    if args.comic_id:
+        target_url = '%s%s' % (WEBSITE_URL, args.comic_id)
+    else:
+        target_url = WEBSITE_URL
+        
+    save_location_img = args.file
 
     print 'requesting %s' % target_url
     url = urllib2.urlopen(target_url)
-    html_data = url.read()
 
     print 'parsing HTML information'
     parser = MemHTMLParser()
-    parser.feed(html_data)
+    parser.feed(url.read())
 
+    # Traverse the HTML in an EXPECTED format
     if 'div' in parser.nodes:
         filtered_divs = [node for node in parser.nodes['div'] if 'id' in node.attributes and node.attributes['id']=='comic']
 
         if len(filtered_divs) > 0:
             # Get the Image URL
-            image_url = '%s' % (filtered_divs[0].children[0].attributes['src'])
+            image_url = filtered_divs[0].children[0].attributes['src']
             request = urllib2.urlopen(image_url)
 
             print 'downloading %s (%s bytes)' % (image_url, request.headers['Content-Length'])
@@ -33,4 +44,4 @@ if __name__ == '__main__':
 
             print 'Successfully downloaded the Picture of the Day to %s' % save_location_img
     else:
-        print 'There was an error locating the background image to download'
+        print 'There was an error locating the image to download (Unexpected format)'
