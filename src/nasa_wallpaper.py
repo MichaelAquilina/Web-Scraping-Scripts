2e116a6324db8ebf4a3fc55713615d31b1481e49
KillaW0lf04 <michaelaquilina@gmail.com>
2013-01-10 14:00:22 +0100

    Moved HTMLParser code to a seperate file

--- src/nasa_wallpaper.py
+++ src/nasa_wallpaper.py
@@ -9,96 +9,9 @@ Created on Jan 9, 2013
 import sys
 import urllib2
 from datetime import datetime
-from HTMLParser import HTMLParser
+from MemHTMLParser import MemHTMLParser
 
 WEBSITE_URL = 'http://apod.nasa.gov/apod'
-
-def get_data(html_node):
-    """
-    Retrieves and extracts the string data contained in the
-    HTML node passed in the parameter. The process is recursive
-    so the entire string data will be returned.
-    """
-    data = _extract_data(html_node)
-    
-    str_buffer = ''
-    for item in data:
-        str_buffer += item
-        
-    return str_buffer.lstrip()
-
-def _extract_data(html_node, data=[]):
-    for child in html_node.children:
-        if type(child) == str:
-            data.append(child
-                        .replace('\n',' ')      # Remove all occurrances of new lines
-                        .strip()                # Remove white spaces at the end
-                        .lstrip()               # Remove white spaces at the front
-                        )
-        else:
-            data.append(' ')
-            _extract_data(child, data)
-            data.append(' ')
-            
-    return data
-
-class HTMLNode(object):
-    """
-    Class representation of an HTML Node that would be found in a
-    typical HTML file.
-    """
-    
-    def __init__(self, tag, pos=-1):
-        self.tag = tag
-        self.children = []
-        self.attributes = {}
-        self.parent = None
-        self.pos = pos
-        
-    def __repr__(self):
-        return '<{tag}: Attributes={attributes}, Pos={pos}>'.format(
-            tag=self.tag,
-            pos=self.pos,
-            attributes=len(self.attributes))
-
-class MemHTMLParser(HTMLParser):
-    """
-    Builds an in-memory representation of a given HTML file through the use of
-    HTMLNode objects that are easily accessible through the nodes instance variable.
-    The structure built by this class assumes XML compliance (TODO: allow non-XML)
-    """
-    
-    def __init__(self):
-        HTMLParser.__init__(self)
-        self.nodes = {}
-        self._node_stack = []
-        
-    def _peek_stack(self):
-        return self._node_stack[len(self._node_stack)-1]
-    
-    def handle_starttag(self, tag, attrs):
-        node = HTMLNode(tag, self.getpos())
-        node.attributes = dict(attrs)
-        
-        if tag not in self.nodes:
-            self.nodes[tag] = []
-        self.nodes[tag].append(node)
-        
-        if len(self._node_stack) > 0:
-            # Append to the item at the top of the stack
-            parent = self._peek_stack()
-            parent.children.append(node)
-            node.parent = parent
-            
-        # Push the new node on the top of the stack for tracking
-        self._node_stack.append(node)
-    
-    def handle_data(self, data):
-        self._peek_stack().children.append(data)
-    
-    def handle_endtag(self, tag):
-        # The node is no longer in context
-        self._node_stack.pop()
 
 if __name__ == '__main__':
     save_location_folder = None
@@ -152,7 +65,7 @@ if __name__ == '__main__':
     
     if expl_list:
         info_file = open(save_location_info, 'w')
-        info_file.write(get_data(expl_list[0].parent))
+        info_file.write(expl_list[0].parent.get_data())
         info_file.close()
     
     if 'img' in parser.nodes:
