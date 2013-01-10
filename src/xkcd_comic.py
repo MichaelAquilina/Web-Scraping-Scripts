e2a62481cb0bee1f49977fe8b5207e4d3b637cdb
KillaW0lf04 <michaelaquilina@gmail.com>
2013-01-10 20:20:56 +0100

    Added 'Random' and 'Info' switch. Improved code style

--- src/xkcd_comic.py
+++ src/xkcd_comic.py
@@ -3,6 +3,11 @@ __author__ = 'michaela'
 from MemHTMLParser import MemHTMLParser
 import urllib2
 import argparse
+import random
+import sys
+
+MAX = 3000
+MAX_ATTEMPTS = 10
 
 WEBSITE_URL = 'http://xkcd.com/'
 
@@ -10,18 +15,38 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser("Download XKCD Comics over command line")
     parser.add_argument('-f', '--file', type=str, required=True, help="Specify the filename to save to")
     parser.add_argument('-c', '--comic-id', type=int, required=False, help="Specify a specific comic to download by id")
+    parser.add_argument('-r', '--random', action="store_true", required=False, help="Download a random comic from XKCD")
+    parser.add_argument('-i', '--info', type=str, required=False, help="Save the alternate text to a user-specified text file")
     
     args = parser.parse_args()
     
-    if args.comic_id:
-        target_url = '%s%s' % (WEBSITE_URL, args.comic_id)
-    else:
-        target_url = WEBSITE_URL
+    retry = True
+    attempts = 0
+    
+    while retry and attempts < MAX_ATTEMPTS:
+        try:
+            retry = False
         
-    save_location_img = args.file
-
-    print 'requesting %s' % target_url
-    url = urllib2.urlopen(target_url)
+            if args.comic_id:
+                target_url = '%s%s' % (WEBSITE_URL, args.comic_id)
+            elif args.random:
+                random_id = random.randint(1,MAX)
+                target_url = '%s%s' % (WEBSITE_URL, random_id)
+            else:
+                target_url = WEBSITE_URL
+                
+            save_location_img = args.file
+        
+            print 'requesting %s' % target_url
+            url = urllib2.urlopen(target_url)
+        except urllib2.URLError:
+            retry = True
+            attempts += 1
+    
+    # Unable to download HTML file; print error and exit
+    if attempts == MAX_ATTEMPTS:
+        print 'Unable to download comic of the day from XKCD'
+        sys.exit()
 
     print 'parsing HTML information'
     parser = MemHTMLParser()
@@ -33,15 +58,27 @@ if __name__ == '__main__':
 
         if len(filtered_divs) > 0:
             # Get the Image URL
-            image_url = filtered_divs[0].children[0].attributes['src']
-            request = urllib2.urlopen(image_url)
+            img_tag = filtered_divs[0].children[0]
+            img_url = img_tag.attributes['src']
+            alt_text = img_tag.attributes['alt']
+            title_text = img_tag.attributes['title']
+            
+            request = urllib2.urlopen(img_url)
 
-            print 'downloading %s (%s bytes)' % (image_url, request.headers['Content-Length'])
+            print 'downloading %s (%s bytes)' % (img_url, request.headers['Content-Length'])
 
             image_file = open(save_location_img,'wb')
             image_file.write(request.read())
             image_file.close()
+            
+            # Show the Comics alternate text
+            info = '\'%s\': %s' % (alt_text, title_text)
+            print info
+            if args.info:
+                info_file = open(args.info, 'w')
+                info_file.write(info)
+                info_file.close()
 
-            print 'Successfully downloaded the Picture of the Day to %s' % save_location_img
+            print 'Successfully downloaded and saved the Picture of the Day to %s' % save_location_img
     else:
         print 'There was an error locating the image to download (Unexpected format)'
